1adfc89d796bf06264c9cbc85c7537fb2471d7b5
Hannah Angle <hannahcangle@gmail.com>
2020-08-08 03:16:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,33 +1,33 @@
-class Dog 
+class Dog
   @@all = []
-  
+
   def initialize(name)
     @name = name
     self.save
   end
-  
-  def self.all 
+
+  def self.all
     @@all
   end
-  
+
   def self.clear_all
-    @@all = []
+    @@all = [] 
   end
-  
-  def self.print_all 
+
+  def self.print_all
     @@all.each do |dog|
       puts dog.name
     end
+end
+
+  def save
+    @@all << self
   end
-  
-  def save 
-    @@ all << self 
-  end
-  
+
   def name
     @name
   end
-  
-end
+
+end 
   
   
